3813a026c5783ef50e072e7ae68b2ab5dbaf116a
TSANG Sze Sze, CC <tsangszesze@icloud.com>
2024-05-15 14:54:12 +0800

    ci: add cors config

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,8 @@
 export const port = process.env.PORT || 8000;
 export const origin = process.env.ORIGIN || `http://localhost:${exports.port}`;
+const localClient = "http://localhost:3000";
+
+export const corsOptions = {
+  origin: [origin, localClient],
+  optionsSuccessStatus: 200,
+};

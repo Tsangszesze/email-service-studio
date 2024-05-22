53cd84d1cff94399505b0e4f77c9199ccae14060
TSANG Sze Sze, CC <tsangszesze@icloud.com>
2024-05-22 19:57:31 +0800

    doc: update doc and deploy script

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./client/**/*.{html,js}"],
+  content: ["./client/**/*.{html,js}", "./docs/**/*.{html,js}"],
   theme: {
     extend: {
       colors: {
